19b11014639ced3b5060cf4a87bc4e6233dfbddc
Ogulcan <junkchorn@gmail.com>
2018-02-07 15:18:06 +0300

    Added main contract and presenter

---
+++ app/src/main/java/com/jibble/test/ui/main/MainPresenter.kt
@@ -0,0 +1,25 @@
+package com.jibble.test.ui.main
+
+/**
+ * Created by ogulcan on 07/02/2018.
+ */
+class MainPresenter: MainContract.Presenter {
+
+    override fun onDrawerOptionAboutClick() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun subscribe() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun unsubscribe() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun attach(view: MainContract.View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
