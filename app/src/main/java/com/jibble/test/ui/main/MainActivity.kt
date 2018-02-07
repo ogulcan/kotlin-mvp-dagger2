86c0bc8d7a85bdcff8c25a6379e66aee1f395756
Ogulcan <junkchorn@gmail.com>
2018-02-07 13:34:31 +0300

    Added main activity and layout

--- app/src/main/java/com/jibble/test/ui/main/MainActivity.kt
+++ app/src/main/java/com/jibble/test/ui/main/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.jibble.test.ui.main
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jibble.test.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 /**
  * Created by ogulcan on 07/02/2018.
  */
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        test()
+    }
+
+    fun test() {
+        hello.setText("Hello world with kotlin extensions")
+    }
+}
