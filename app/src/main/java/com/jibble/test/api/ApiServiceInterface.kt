17d13f53317b9f4f12d8557d7cd0c81f7588b4db
Ogulcan <junkchorn@gmail.com>
2018-02-07 11:53:25 +0300

    Updated api service w/ adapter factory

--- app/src/main/java/com/jibble/test/api/ApiServiceInterface.kt
+++ app/src/main/java/com/jibble/test/api/ApiServiceInterface.kt
@@ -6,6 +6,7 @@ import com.jibble.test.models.User
 import com.jibble.test.util.Constants
 import io.reactivex.Observable
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -36,6 +37,7 @@ interface ApiServiceInterface {
     companion object Factory {
         fun create(): ApiServiceInterface {
             val retrofit = retrofit2.Retrofit.Builder()
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create())
                     .baseUrl(Constants.BASE_URL)
                     .build()
