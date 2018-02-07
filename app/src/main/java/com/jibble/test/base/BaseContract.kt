04874bb41596e463c835075e7c9d8bc342712d9a
Ogulcan <junkchorn@gmail.com>
2018-02-07 13:35:21 +0300

    Added BaseContract as an initial step of MVP implementation

---
+++ app/src/main/java/com/jibble/test/base/BaseContract.kt
@@ -0,0 +1,17 @@
+package com.jibble.test.base
+
+/**
+ * Created by ogulcan on 07/02/2018.
+ */
+class BaseContract {
+
+    interface Presenter<in T> {
+        fun subscribe()
+        fun unsubscribe()
+        fun attach(view: T)
+    }
+
+    interface View {
+
+    }
+}
