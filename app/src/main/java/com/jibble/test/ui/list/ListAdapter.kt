11ff7873d500d1fb4183f66036689188c42cb1fb
Ogulcan <junkchorn@gmail.com>
2018-02-08 13:56:26 +0300

    Added list item layout and implemented viewholder

--- app/src/main/java/com/jibble/test/ui/list/ListAdapter.kt
+++ app/src/main/java/com/jibble/test/ui/list/ListAdapter.kt
@@ -1,31 +1,63 @@
 package com.jibble.test.ui.list
 
 import android.content.Context
+import android.support.constraint.ConstraintLayout
 import android.support.v4.app.Fragment
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jibble.test.models.User
+import android.widget.TextView
+import com.jibble.test.R
+import com.jibble.test.models.Post
 
 /**
  * Created by ogulcan on 07/02/2018.
  */
-class ListAdapter(private val context: Context, private val list: MutableList<User>,
-                  fragmet: Fragment): RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+class ListAdapter(private val context: Context, private val list: MutableList<Post>,
+                  fragment: Fragment): RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
+
+    private val listener: ListAdapter.onItemClickListener
+
+    init {
+        this.listener = fragment as ListAdapter.onItemClickListener
+    }
+
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onBindViewHolder(holder: ListViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var post = list[position]
+
+        // holder!!.bind(post)
+        holder!!.title!!.setText(post.title)
+        holder.body!!.setText(post.body)
+
+        holder.layout!!.setOnClickListener {
+            listener.itemDetail(post.id.toString()!!)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ListViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val itemView = LayoutInflater.from(context).inflate(R.layout.item_layout, parent, false)
+        return ListAdapter.ListViewHolder(itemView)
     }
 
     class ListViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        var layout = itemView.findViewById<ConstraintLayout>(R.id.item_layout)
+        val title = itemView.findViewById<TextView>(R.id.item_title)
+        val body = itemView.findViewById<TextView>(R.id.item_body)
 
+        fun bind(item: Post) {
+            // title = item.post
+            // body etc.
+        }
+    }
+
+    interface onItemClickListener {
+        fun itemRemoveClick(post: Post)
+        fun itemDetail(postId : String)
     }
 }
